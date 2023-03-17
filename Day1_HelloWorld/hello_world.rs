0774cd5332253d287d8e6085a918d454c490da68
jcomfort1 <joshuacomfort@gmail.com>
2023-03-17 13:13:45 -0400

    Completed example 1, Hello World

---
+++ Day1_HelloWorld/hello_world.rs
@@ -0,0 +1,9 @@
+// Name: Joshua Comfort
+// Date: 3/17/2023
+// Example Rust program to print the string "Hello World!" to the console
+// Done as part of learning learning the Rust programming language
+
+fn main()
+{
+    println!("Hello World!");
+}
--- hello_world.rs
+++
@@ -1,6 +0,0 @@
-// Name: Joshua Comfort
-// Date: 3/17/2023
-// Example Rust program to print the string "Hello World!" to the console
-// Done as part of learning learning the Rust programming language
-
-
